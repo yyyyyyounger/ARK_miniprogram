a019ad2cb2a3dee4ea5f91f6b86cf5be76a97657
yyyyyyounger <1049825685@qq.com>
2021-06-17 02:10:47 +0800

    update

---
+++ GithubInit.sh
@@ -0,0 +1,7 @@
+git config --global user.name "yyyyyyounger"
+git config --global user.email "1049825685@qq.com"
+
+echo " "
+echo "The program has finished ~"
+echo " "
+read -p "Press \"Enter\" to End"
