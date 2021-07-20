923f6dcb2e1c5a6739f632f86f9a663ac95cc04a
yyyyyyounger <1049825685@qq.com>
2021-07-20 23:15:14 +0800

    update

--- cloud/userSignUp/index.js
+++ cloud/userSignUp/index.js
@@ -16,10 +16,14 @@ exports.main = async (event, context) => {
   let date = new Date(Date.parse(new Date()));    // Date.parse(new Date()) 和 Date.now()為當前時間戳 - 數字。new Date(時間戳)後化為帶有中文的字符串
   let today = date.toLocaleDateString();
 
+  let userInfoTemp = event.userInfoInput;
+  let arkIdIndex = userInfoTemp.findIndex(o=> o.shortName === 'arkId' ); // 準備插入最新arkid
+
   // 查找最新的arkid
   await db.collection('user') .orderBy("arkid","desc") .field({ arkid:true }) .limit(1) .get()
     .then(res=>{
       // console.log("最新arkid為",res.data[0].arkid);
+      userInfoTemp[arkIdIndex].input = '00'+((parseInt(res.data[0].arkid)+1)+"");   // 插入最新的arkid
       db.collection('user').add({   // 對 user 集合插入新用戶的數據
         data: {
           _id         : wxContext.OPENID,
@@ -32,8 +36,13 @@ exports.main = async (event, context) => {
           nickName    : event.nickName,
           gender      : event.gender,
           // 前端傳入的用戶個人填寫的註冊信息
-          userInfoInput : event.userInfoInput,    // 前端更新isSignUp的狀態 - 但不夠安全
+          userInfoInput : userInfoTemp,    // 前端更新isSignUp的狀態 - 但不夠安全
         }
+      }) .then(res=>{
+        let arkIdIndex = event.userInfoInput.findIndex(o=> o.shortName === 'arkId' );
+        db.collection('user').doc(wxContext.OPENID).update({
+          'userInfoInput.userInfoInput.input' : res.data[0].arkid+1
+        })
       })
     }) .catch(res=>{    console.error(res);  })
 
