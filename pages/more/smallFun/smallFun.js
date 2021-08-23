824ab3eb7abe359ffbf76b05bbd1e2ab72a87ac5
ImK4Lok <samkalok107@gmail.com>
2021-08-24 01:14:03 +0800

    add bus checking function

--- pages/more/smallFun/smallFun.js
+++ pages/more/smallFun/smallFun.js
@@ -1,6 +1,10 @@
+let util = require('../../../utils/util');
+
 Page({
   data: {
-    
+    datetime: "yyyy-mm-ddThh:mm:ss+08:00",
+    vehiclePlateNumber: "",
+    station: ""
   },
   onLoad: function() {
     
@@ -33,5 +37,40 @@ Page({
 
   },
   
+  btnClick:function(){
+    var that = this;
+    var start_time = util.formatTime_start_bus_api(new Date());
+    var end_time = util.formatTime_end_bus_api(new Date());
+    console.log('start time', start_time);
+    console.log('end time', end_time);
+    wx.request({
+      url: `https://api.data.um.edu.mo/service/facilities/shuttle_bus_arrival_time/v1.0.0/all?date_from=${start_time}&date_to=${end_time}`,
+      data: {},
+      header: {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer 44f6acc1-df55-30d8-a30e-ac84403b05b5'
+      },
+      success(res){
+        console.log(res.data);
+        if(res.data._returned == 0){
+          console.log("returned 0");
+          that.setData({
+            datetime: "yyyy-mm-ddThh:mm:ss+08:00",
+            station: "error",
+            vehiclePlateNumber: "error"
+          })
+        }else{
+          that.setData({
+            datetime: res.data._embedded[0].datetime,
+            station: res.data._embedded[0].station,
+            vehiclePlateNumber: res.data._embedded[0].vehiclePlateNumber
+          })
+        }
+      },
+      fail(res){
+        console.log(res);
+      }
+    })
+  } 
 });
   
