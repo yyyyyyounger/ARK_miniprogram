292ca06c4adce706414e9c4ee3e807efcf70f924
yyyyyyounger <1049825685@qq.com>
2021-08-26 13:04:45 +0800

    準備開始用戶詳情頁

--- pages/user/userDetail/userDetail.js
+++ pages/user/userDetail/userDetail.js
@@ -1,66 +1,62 @@
-// pages/user/userDetail/userDetail.js
-Page({
+const db = wx.cloud.database();   // 數據庫
+const _ = db.command
 
-  /**
-   * 页面的初始数据
-   */
+
+Page({
   data: {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    this.app = getApp();
+    // 獲取上個頁面傳遞的參數，說明用戶組和需要渲染的courseId
+    let detailInfo = JSON.parse(options.detailInfo);
+    console.log("上個頁面傳遞值為：",detailInfo)
+    console.log("arkid為",detailInfo.arkid)
+
+    // detailInfo.arkid = 2;   // 可以修改傳參的arkid
+
+    // 獲取數據庫數據
+    db.collection('user').where({
+      arkid : detailInfo.arkid
+    }) .field({
+      avatarUrl      : true,
+      gender         : true,
+      userInfoInput  : true,
+    }) .get() .then(res=>{
+      let userInfo = res.data[0]
+      console.log(userInfo);
+      // 這裡開始寫處理的代碼，比如setData進data中給wxml渲染
+    })
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  // 頁面跳轉
+  jumpToCourseDetail (e){
+    let arkid = e.currentTarget.dataset.courseid;
+
+    // 跳轉課程詳情頁
+    let detailInfo = {
+      arkid  : arkid,
+    }
+
+    detailInfo = JSON.stringify(detailInfo);
+    console.log(detailInfo);
+    wx.navigateTo({
+      url: './courseDetail/courseDetail?detailInfo=' + detailInfo,
+    })
+  },
 })
