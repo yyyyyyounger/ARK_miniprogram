a921602088a395fe0ca269115ca37b76527d3a98
keltam27 <k736812950@gmail.com>
2021-08-29 18:26:12 +0800

    更新小功能頁

--- pages/more/smallFun/smallFun.js
+++ pages/more/smallFun/smallFun.js
@@ -1,7 +1,32 @@
 
 Page({
   data: {
-
+    funs:[
+      {
+        title:'澳大巴士報站',
+        icon:'/icons/outline_directions_bus_white_24dp.png',
+        color:'#dd565a',
+        uri:'bus'
+      },
+      {
+        title:'組隊約時間',
+        icon:'/icons/outline_schedule_white_24dp.png',
+        color:'#6d90c6',
+        uri:'meet'
+      },
+      {
+        title:'書院食堂菜單',
+        icon:'/icons/outline_menu_book_white_24dp.png',
+        color:'#6da14e',
+        uri:'menu'
+      },
+      {
+        title:'Deadline提醒',
+        icon:'/icons/outline_pending_actions_white_24dp.png',
+        color:'#d5793e',
+        uri:'deadline'
+      },
+    ]
   },
   onLoad: function() {
 
